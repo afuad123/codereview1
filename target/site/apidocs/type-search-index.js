9b4b9ce166e7fdbbbb4ba574c271bed3b922fba2
afuad727 <afuad@guilford.edu>
2024-02-20 09:48:25 -0500

    javadoc

---
+++ target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"edu.guilford","l":"Driver"},{"p":"edu.guilford","l":"ScrabbleSet"},{"p":"edu.guilford","l":"Tile"}];updateSearchResults();
